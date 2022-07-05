fb5c7cf8a939866cfd249d8dc01b06ef6fb21044
Urgirlempress <stephanieklein734@gmail.com>
2022-07-05 12:10:04 -0700

    Print Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = o; x < 10; x++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n')
+}
