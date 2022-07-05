8464cf99277c543b9c0009fe81244bf24df56add
Urgirlempress <stephanieklein734@gmail.com>
2022-07-05 08:25:52 -0700

    _Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/** 
- * main - prints putchar
+/**
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+  int i = 0;
+  char text[] = "_putchar";
 
-return (0);
+  while (text[i] != '\0')
+  {
+    _putchar(text[i]);
+    i++;
+  }
+  _putchar('\n');
+
+  return (0);
 }
