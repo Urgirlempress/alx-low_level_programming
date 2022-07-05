13b6a0ba73e868dc0edd210a0e101f764a059550
Urgirlempress <stephanieklein734@gmail.com>
2022-07-05 14:33:47 -0700

    Print Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,52 @@
 #include "main.h"
 
 /**
-*times_table -> time table
-*/
-void times_table(void)
-{
-	int x, y, k;
+ * print_times_table - prints time table
+ * @n : times table to use
+ * Description: prints the times table
+ * Return:void
+ */
 
-	for (x = 0; x < 10; x++)
+void print_times_table(int n)
+{
+
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		for (y = 0; y < 10; y++)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			k = x * y;
-			if (y == 0)
-				_putchar(k + '0');
-			if (y != 0 && k < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-		_putchar('\n');
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
+	_putchar('\n');
+	a++;
+}
 }
