7bb8c76555e37359267b458a90b03fb53699a1fe
Urgirlempress <stephanieklein734@gmail.com>
2022-07-05 17:54:06 -0700

    One alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
-
 /**
- * main - Entry begins
- * Return: nothing, return void
+ * main - Entry point
+ *
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i = 0;
+	char text[] = "_putchar";
 
+	while (text[i] != '\0')
+	{
+	_putchar(text[i]);
+ 	i++;
+	}
+	_putchar('\n');
 return (0);
 }
